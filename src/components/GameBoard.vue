920af32e07bb584d4fae6dad8f8b175e3084ba87
Jeremy Chua <jktchua@gmail.com>
2017-12-31 09:37:14 -0800

    Updated GameBoard design and UX

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,63 +1,34 @@
 <template>
   <div id="game_board">
-    <div
-      v-for="(card, index) in cards"
-      @click="$emit('click', index)"
-      :class="`card ${card.type}${card.revealed}`">
-      <b>{{ card.word }}</b>
-    </div>
+    <template v-for="(card, index) in cards">
+      <GameCard
+        @click="$emit('click', index)"
+        :card="card"
+        :index="index"
+      />
+    </template>
   </div>
 </template>
 
 <script>
+import GameCard from './GameCard';
+
 export default {
   name: 'GameBoard',
+  components: {
+    GameCard,
+  },
   props: {
     cards: Array,
   },
 }
 </script>
 
-<style scoped>
+<style>
 #game_board {
   display: flex;
   flex: 1;
   flex-wrap: wrap;
   padding: 10px;
-}
-.card {
-  flex: 1 0 20%;
-  box-sizing: border-box;
-  background: #e0ddd5;
-  color: #171e42;
-  padding: 10px;
-  border: 1px solid black;
-  align-items: center;
-  justify-content: center;
-  display: flex;
-  cursor: pointer;
-}
-.card.bluetrue {
-  background-color: teal;
-}
-.card.redtrue {
-  background-color: tomato;
-}
-.card.wrongtrue {
-  background-color: #b3ab96;
-}
-.card.losetrue {
-  background-color: dimgray;
-}
-.card.bluefalse {
-  background-color: lightcyan;
-  color: teal;
-}
-.card.redfalse {
-  background-color: pink;
-  color: tomato;
-}
-.card.losefalse {
-  background-color: gray;
 }
 </style>
---
+++ src/components/GameCard.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="card-container"
+    :class="`${card.type} ${card.revealed ? 'revealed' : 'unknown'}`">
+    <div
+      @click="$emit('click', index)"
+      class="card">
+      <div class="front face">
+        <b>{{ card.word }}</b>
+      </div>
+      <div class="back face" :class="`${card.type}${card.revealed}`">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameCard',
+  props: {
+    card: Object,
+    index: Number,
+  },
+}
+</script>
+
+<style>
+.card-container {
+  flex: 1 0 20%;
+  perspective: 1000;
+  box-sizing: border-box;
+  justify-content: center;
+  display: flex;
+  padding: 3px;
+}
+.card {
+  background-color: #e0ddd5;
+  color: #171e42;
+  cursor: pointer;
+  flex: 1;
+  transform-style: preserve-3d;
+  transition: all .5s ease-in-out;
+  border: 6px solid white;
+  border-radius: 10px;
+}
+.face {
+  transition: all .5s ease-in-out;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+}
+.blue.revealed .card .back {
+  background-color: teal;
+}
+.red.revealed .card .back {
+  background-color: tomato;
+}
+.wrong.revealed .card .back {
+  background-color: #b3ab96;
+}
+.lose.revealed .card .back {
+  background-color: dimgray;
+}
+.blue.unknown .card .front, .blue.revealed .card .front {
+  background-color: lightcyan;
+  color: teal;
+}
+.red.unknown .card .front, .red.revealed .card .front {
+  background-color: pink;
+  color: tomato;
+}
+.lose.unknown .card .front, .lose.revealed .card .front {
+  background-color: gray;
+}
+
+.revealed .card {
+  transform: rotateY(180deg);
+}
+.revealed:hover .card {
+  transform: rotateY(0deg);
+}
+
+.face {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+}
+.face.back {
+  transform: rotateY(180deg);
+}
+</style>
